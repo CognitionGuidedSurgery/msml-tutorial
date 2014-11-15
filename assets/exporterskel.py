46d0caf8391a0272074d6a3d0908e19df8ccf98b
Alexander Weigl <spamling@web.de>
2014-11-15 11:21:01 +0100

    .

--- assets/exporterskel.py
+++ assets/exporterskel.py
@@ -8,15 +8,19 @@ class ExporterSkeleton(Exporter):
                         output_type_of_elements = ...)
 
     def render(self):
-        #
-        #
-        #
-        #
-        pass
+        self._update = {}
+        for sceneobject in self.datamodel:
+            # process object
+            sceneobject.id
+            sceneobject.mesh
+            for constraints in sceneobject.constraints:  pass
+            for regions in sceneobject.materials: pass
+            for output in sceneobject.output: pass
+
+        # write simulation input file
 
     def execute(self):
-        #
-        #
-        #
-        #
-        pass
+        # execute the simulation
+        subprocess.call(["run_simulation" ...])
+        # return the new memory values
+        return self._update
